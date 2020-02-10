51ad062c9dddafa8beac83bdfee260671ec1848a
happy0088 <abhishek.k0088@gmail.com>
2020-02-10 21:36:22 +0530

    added Person entity with properties

---
+++ src/main/java/com/absonworld/personManagement/entity/Person.java
@@ -0,0 +1,63 @@
+package com.absonworld.personManagement.entity;
+
+import java.util.List;
+
+public class Person {
+
+    String first_name;
+    String last_name;
+    int age;
+    String favourite_color;
+    List<String> hobby;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFavourite_color() {
+        return favourite_color;
+    }
+
+    public void setFavourite_color(String favourite_color) {
+        this.favourite_color = favourite_color;
+    }
+
+    public List<String> getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(List<String> hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", age=" + age +
+                ", favourite_color='" + favourite_color + '\'' +
+                ", hobby=" + hobby +
+                '}';
+    }
+}
